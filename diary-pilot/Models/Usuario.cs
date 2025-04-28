c72a4c79ce64e27ad8116bfe8289b119a488bc95
FlavioMoreira1612 <flaviol.moreira@icloud.com>
2025-04-28 19:27:10 -0300

    configurações gerais do banco de dados

---
+++ diary-pilot/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace diary_pilot.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a senha")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a senha")]
+        public Perfil Perfil { get; set; }
+    }
+
+    public enum Perfil
+    {
+        Profissional,
+        Estudante
+    }
+}
